623af8117b35de1f420dab09147f074ce7004785
ksoheee <thgml0716030@naver.com>
2025-09-29 17:36:22 +0900

    feat: 재고 생성

--- src/main/java/com/hams/gearfirst/controller/PartsController.java
+++ src/main/java/com/hams/gearfirst/controller/PartsController.java
@@ -1,23 +1,28 @@
 package com.hams.gearfirst.controller;
 
 import com.hams.gearfirst.entitiy.Part;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/parts")
 public class PartsController {
+
+    private final List<Part> parts = new ArrayList<>(); // 메모리에 저장
+
     @GetMapping
     public List<Part> getParts() {
-        List<Part> parts = List.of(
-                new Part(1L, "엔진1", 10),
-                new Part(2L, "창문1", 15),
-                new Part(3L, "오일1", 40)
-        );
-
+        parts.add(new Part(1L, "엔진1", 10));
+        parts.add(new Part(2L, "창문1", 15));
+        parts.add(new Part(3L, "오일1", 40));
         return parts;
+    }
+
+    @PostMapping
+    public Part createParts(@RequestBody Part request){
+        parts.add(request);
+        return request;
     }
 }
